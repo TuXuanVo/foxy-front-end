441ac3c39ee38c2c6fa772c7eee1453232194a94
TuXuanVo <tu.vo@zodinet.tech>
2022-08-25 22:33:06 +0700

    chore : update something

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -235,10 +235,7 @@ const Profile: NextPageWithLayout = () => {
                                             </button>
                                         </li>
                                         <li>
-                                            <button
-                                                className="flex items-center justify-start w-full gap-3 py-1 pl-2 rounded-md text-primary-50 button-2 bg-slate-100"
-                                                onClick={onEnterGift}
-                                            >
+                                            <button className="flex items-center justify-start w-full gap-3 py-1 pl-2 rounded-md text-primary-50 button-2 bg-slate-100">
                                                 <BsCoin />
                                                 <p>Coins package</p>
                                             </button>
