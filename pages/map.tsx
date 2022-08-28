24ecc5d3c8a324c10be15352574a61e46ad5e256
TuXuanVo <tu.vo@zodinet.tech>
2022-08-28 11:10:26 +0700

    chore: update get location

--- pages/map.tsx
+++ pages/map.tsx
@@ -65,7 +65,7 @@ const MapContainer: NextPageWithLayout = () => {
     };
 
     useEffect(() => {
-        handlePermission();
+        initGeolocation();
         return () => {
             setFriends([]);
         };
